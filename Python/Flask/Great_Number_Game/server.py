0e28e927fa6f4d3a03bc7ef007c2a0e6e3de0e44
Colin Jao <jao.colin@gmail.com>
2017-04-07 15:07:52 -0700

    Removed global variables from Great Number Game

--- Python/Flask/Great_Number_Game/server.py
+++ Python/Flask/Great_Number_Game/server.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 app.secret_key = 'colin'
 
-first_visit = True
-numberToGuess = random.randint(1,100)
+# first_visit = True
+# numberToGuess = random.randint(1,100)
 
 @app.route('/')
 def renderMain():
-	global first_visit
-	global numberToGuess 
+	# global first_visit
+	# global numberToGuess 
 
-	if first_visit == True:
-		session['numberToGuess'] = numberToGuess
+	if not 'numberToGuess' in session:
+		session['numberToGuess'] = random.randint(1,100)
 		print session['numberToGuess']
 		session['proximity'] = ''
 		session['numberOfGuesses'] = 0
-		first_visit = False
+		# first_visit = False
 
 	return render_template('index.html', numberToGuess=session['numberToGuess'], proximity=session['proximity'], numberOfGuesses=session['numberOfGuesses'])
 
@@ -43,10 +43,9 @@ def processGuess():
 
 @app.route('/reset', methods=['POST'])
 def reset():
-	global first_visit
-	global numberToGuess
-	numberToGuess = random.randint(1,100)
-	first_visit = True
+	# global first_visit
+	# global numberToGuess
+	del session['numberToGuess']
 	return redirect('/')
 
 app.run(debug=True)
